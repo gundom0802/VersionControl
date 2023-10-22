94e449ad12ccfb6caa6b87774254bad7d7d206d3
3clipse <gundom0802@gmail.com>
2023-10-21 20:10:13 -0400

    Incorporated new return functions

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-# Matthew Dam Encoder function
-def encode():
-    # Converts input string into list
-    password = list(input("Please enter your password to encode: "))
+# Matthew Dam
+def encode(password):
     # Loops through length of password list
     for i in range(len(password)):
         # Changes index i of password and adds 3
         password[i] = str(int(password[i]) + 3)[-1]
     # Joins all elements of the list into a new string
-    password = "".join(password)
-    print("Your password has been encoded and stored!\n")
+    global encoded_password
+    encoded_password = "".join(password)
+    return password
 
 
 if __name__ == '__main__':
+    # declare initial password
+    password = ""
     # Looping until quit
     while True:
         # Displays menu
         sel = input("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n\nPlease enter an option: ")
+        # Encodes password
         if sel == "1":
-            encode()
+            password = encode(list(input("Please enter your password to encode: ")))
+            print("Your password has been encoded and stored!\n")
         if sel == "2":
-            pass # REPLACE "PASS" WITH DECODER FUNCTION CALL
+            pass  # DECODE PASSWORD FUNCTION HERE
         if sel == "3":
             break
