89c0efddc751f2a4d2f619e9bd45b1967d06f62b
Denglish66 <148647088+Denglish66@users.noreply.github.com>
2023-10-24 21:11:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
 # Matthew Dam
-def encode (password):
-   encoded_password = ""
-   for number in password:
-       added_number = str((int(number) + 3) % 10)  # add each number by 3
-       encoded_password += added_number
-   return encoded_password
-
-
-def decode (encoded_password):
-   password = ""
-   for number in encoded_password:
-       subtracted_number = str((int(number) - 3) % 10)  # subtract each number by 3
-       password += subtracted_number
+def encode(password):
+   # Loops through length of password list
+   for i in range(len(password)):
+       # Changes index i of password and adds 3
+       password[i] = str(int(password[i]) + 3)[-1]
+   # Joins all elements of the list into a new string
+   global encoded_password
+   encoded_password = "".join(password)
    return password
+
+
+def decode(password):
+   for i in range(len(password)):
+       password[i] = str(int(password[i]) - 3)
+       if int(password[i]) < 0:
+           password[i] = str(int(password[i] + 10))
+   password = "".join(password)
+   print(f"The encoded password is {encoded_password}, and the original password is {password}.")
+
+
 def main():
-    while True:
-        print("Menu")
-        print("-------------")
-        print("1. Encode")
-        print("2. Decode")
-        print("3. Quit")
-        print("")
-        choice = input("Please enter an option: ")
+   # declare initial password
+   password = ""
+   # Looping until quit
+   while True:
+       # Displays menu
+       sel = input("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n\nPlease enter an option: ")
+       # Encodes password
+       if sel == "1":
+           password = encode(list(input("Please enter your password to encode: ")))
+           print("Your password has been encoded and stored!\n")
+       if sel == "2":
+           decode(password)
+       if sel == "3":
+           break
 
-        if choice == "1":
-            password = input("Please enter your password to encode: ")
-            encoded_password = encode(password)
-            print("Your password has been encoded and stored!")
-            print("")
+
+if __name__ == '__main__':
+   main()
 
 
-        elif choice == "2":
-            print(
-                f"The encoded password is {encoded_password}, and the original password is {decode(encoded_password)}.")
-            print("")
-        elif choice == "3":
-            break
 
 
 
